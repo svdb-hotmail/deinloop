6cf295ca2a1331675fab420a20c9e8009689dc64
svdb-hotmail <146592644+svdb-hotmail@users.noreply.github.com>
2023-10-06 01:28:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,16 @@
-// Dark and Light Mode Detection and Implementation
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+// Detect user's preferred theme
+const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-const body = document.getElementById("body");
-const html = document.getElementById("html");
+if (userPrefersDark) {
+    document.getElementById('html').classList.add('dark');
+    document.getElementById('body').classList.add('dark');
+}
 
-if (prefersDarkScheme.matches) {
-    body.classList.add("bg-black", "text-white");
-    html.querySelectorAll(".title-color").forEach(el => {
-        el.classList.replace("title-color", "text-orange-500");
+// Function to change language
+function changeLanguage(lang) {
+    const textElements = document.querySelectorAll('[data-lang]');
+    textElements.forEach(el => {
+        el.textContent = el.getAttribute(`data-${lang}`);
     });
 }
 
-// Language Selection Button Logic
-const langButton = document.getElementById("langButton");
-
-const enText = {
-    title: "Your Support, Their Future",
-    subtitle: "Social Walk-In Center ‘DE INLOOP’ in Sint-Niklaas",
-    // ... More content
-};
-
-const nlText = {
-    title: "Jouw Steun, Hun Toekomst",
-    subtitle: "Sociaal inloopcentrum 'DE INLOOP' in Sint-Niklaas",
-    // ... More content
-};
-
-let currentLang = "en";
-
-langButton.addEventListener("click", () => {
-    if (currentLang === "en") {
-        currentLang = "nl";
-        langButton.textContent = "Nederlands";
-    } else {
-        currentLang = "en";
-        langButton.textContent = "English";
-    }
-
-    Object.keys(enText).forEach(key => {
-        const el = document.querySelector(`[data-lang="${key}"]`);
-        el.textContent = currentLang === "en" ? enText[key] : nlText[key];
-    });
-});
